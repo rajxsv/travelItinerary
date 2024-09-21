70bad551045af0f4606bb9d507bcb2e0871c9fa5
Rajpreet Singh <rajpreetsingh@Rajpreets-MacBook-Pro.local>
2024-09-22 02:17:58 +0530

    Removed vite,react logo files and format code

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,29 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import Lottie from 'react-lottie';
-import animationData from '../../public/Animation - 1726768680248.json'; 
+import React, { useEffect, useState } from "react";
+import Lottie from "react-lottie";
+import animationData from "../../public/Animation - 1726768680248.json";
 
 const Loader = () => {
-    const [show,setshow] = useState(false)
+  const [show, setshow] = useState(false);
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            setshow(true)
-        },5*1000)
-    },[])
+  useEffect(() => {
+    setTimeout(() => {
+      setshow(true);
+    }, 5 * 1000);
+  }, []);
 
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
+      preserveAspectRatio: "xMidYMid slice",
     },
   };
 
   return (
-    <div className='w-full h-svh flex flex-col justify-center items-center'>
+    <div className="w-full h-svh flex flex-col justify-center items-center">
       <Lottie options={defaultOptions} height={200} width={400} />
-      {show && <span>Due to free tier backend service, response may take time initially. Please be patient.</span>}
+      {show && (
+        <span>
+          Due to free tier backend service, response may take time initially.
+          Please be patient.
+        </span>
+      )}
     </div>
   );
 };
